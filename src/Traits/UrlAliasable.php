64a7f1bea9cfe31ab25670ebcca4f97f00546e64
Fomin Vasil <fomvasss@gmail.com>
2020-07-30 11:41:26 +0300

    Add locale scopes

--- src/Traits/UrlAliasable.php
+++ src/Traits/UrlAliasable.php
@@ -41,4 +41,33 @@ trait UrlAliasable
         
         return null;
     }
+
+
+    /**
+     * @param $query
+     * @param null $locale
+     * @return mixed
+     */
+    public function scopeByLocale($query, $locale = null)
+    {
+        $locale = $locale ?: \UrlAliasLocalization::getCurrentLocale();
+
+        return $query->whereLocale($locale);
+    }
+    
+
+    /**
+     * @param $query
+     * @param null $locales
+     * @return mixed
+     */
+    public function scopeByLocales($query, $locales = null)
+    {
+        $locales = $locales ?: session('app_locales');
+
+        if ($locales) {
+            return $query->whereIn('locale', is_array($locales) ? $locales : [$locales]);
+        }
+    }
+
 }
